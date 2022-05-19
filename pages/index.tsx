c2759aed97c273483a38c2fe74684af2f29b84bb
laira <laira@whitewidget.com>
2022-05-19 21:43:03 +0800

    render isr-rendered data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
 import useSWR from 'swr';
 import { gql } from '@apollo/client';
 
@@ -43,9 +42,15 @@ interface IHome {
 }
 
 const Home = ({ comments }: IHome) => {
+  const [commentsState, setCommentsState] = useState<any[] | null>(comments);
   const { data, error } = useSWR(query, getData);
 
-  useEffect(() => console.log('client', data, comments), [data]);
+  useEffect(() => {
+    if (data) {
+      setCommentsState(data.comments);
+    }
+  }, [data]);
+  // useEffect(() => console.log('commentsState', commentsState), [commentsState]);
 
   return (
     <div className={styles.container}>
@@ -56,62 +61,12 @@ const Home = ({ comments }: IHome) => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to
-          {' '}
-          <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing
-          {' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {
+          commentsState && commentsState.map((comment: any) => (
+            <div key={comment.id}>{ comment.content }</div>
+          ))
+        }
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by
-          {' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
